7b898feb5b55c575ef28abf2b32ed77313cdaa42
inmsong <42826759+inmsong@users.noreply.github.com>
2021-07-13 18:24:24 -0500

    prueba

---
+++ envia.php
@@ -0,0 +1,31 @@
+﻿<?php
+$remitente = $_POST['email'];
+$destinatario = 'contacto@gurl-gomez-sosa.com.mx'; // en esta línea va el mail del destinatario.
+$asunto = 'Consulta'; // acá se puede modificar el asunto del mail
+if (!$_POST){
+?>
+
+<?php
+}else{
+	 
+    $cuerpo = "Nombre: " . $_POST["nombre"] . "\r\n"; 
+    $cuerpo = "Apellido Paterno: " . $_POST["paterno"] . "\r\n";
+    $cuerpo = "Apellido Materno: " . $_POST["materno"] . "\r\n";
+    $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
+    $cuerpo .= "Teléfono: " . $_POST["telefono"] . "\r\n";
+	$cuerpo .= "Consulta: " . $_POST["consulta"] . "\r\n";
+	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo. 
+	// Si se agrega un campo al formulario, hay que agregarlo acá.
+
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['paterno']." ".$_POST['apellido']."\" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+    
+    include 'confirma.html'; //se debe crear un html que confirma el envío
+}
+?>
